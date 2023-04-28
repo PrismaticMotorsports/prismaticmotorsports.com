f23087386f6abb1540f6bbe5a99ef4d2f130cd7f
Roxie Gibson <me@roxxers.xyz>
2023-04-28 22:15:35 +0100

    refactor: sponsor compontent

---
+++ src/components/sponsors.tsx
@@ -0,0 +1,24 @@
+import sponsors from '@/data/sponsors'
+import Image from 'next/image'
+
+export default () => {
+    return <div className="max-w-screen-2xl">
+    <div className="flex flex-col items-center font-pp">
+      <h1 className='text-4xl'>Our Partners</h1>
+    </div>
+    <div className="flex flex-wrap flex-row mt-6 justify-center content-center gap-2 w-full">
+      {sponsors.map((sponsor, i) => {
+        return <div key={i} className="flex items-center py-3 bg-white xl:w-1/5 lg:w-1/4 sm:w-1/3 h-40 p-5 mt-auto">
+          <Image
+            style={{objectFit: 'contain'}}
+            className='w-full h-full'
+            width={1000}
+            height={500}
+            src={`/img/sponsors/${sponsor.short_name}.png`}
+            alt={`${sponsor.name} Logo`}
+          />
+        </div>
+      })}
+    </div>
+  </div>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,19 @@
-import sponsors from '@/data/sponsors'
-import Image from 'next/image'
+import Sponsors from "@/components/sponsors";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
       <div className="mt-auto mb-auto text-white">
-        <h1 className="font-pp md:text-7xl sm:text-6xl text-4xl">
+        <h1 className="font-pp sm:pt-32 md:pt-64 md:text-7xl sm:text-6xl text-4xl">
           Prismatic Motorsports
         </h1>
         <h2 className="md:text-3xl sm:text-xl">
           Coming Soon
         </h2>
       </div>
-      <div className="mt-auto max-w-screen-2xl">
-        <div className="flex flex-col items-center font-pp">
-          <h1 className='text-4xl'>Our Partners</h1>
-        </div>
-        <div className="flex flex-wrap flex-row mt-6 justify-center content-center gap-2 w-full">
-          {sponsors.map((sponsor, i) => {
-            return <div key={i} className="flex items-center py-3 bg-white xl:w-1/5 lg:w-1/4 h-40 p-5 mt-auto">
-              <Image
-                style={{objectFit: 'contain'}}
-                width={1000}
-                height={500}
-                src={`/img/sponsors/${sponsor.short_name}.png`}
-                alt={`${sponsor.name} Logo`}
-              />
-            </div>
-          })}
-        </div>
+      <div className="mt-auto pt-32">
+        <Sponsors/>
       </div>
     </main>
   )
