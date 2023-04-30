db1fac2903954cc03f950336396e96e9fc1e9e92
Roxie Gibson <me@roxxers.xyz>
2023-04-30 23:10:49 +0100

    feat: add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return <footer
+        className="p-4 mt-auto flex w-full bg-opacity-20 bg-black text-center text-white">
+            <span className="mx-auto">Website by
+                <a className="hover:text-gray-200" href="https://github.com/roxxers"> Roxie Gibson</a> - Made with 
+                <a href="https://youtu.be/_p_uBOn5oOY?t=8"> 💙</a> - 
+                <a className="hover:text-gray-200" href="https://github.com/PrismaticMotorsports/prismaticmotorsports.com"> Licensed under the AGPL</a>
+            </span>
+    </footer>
+}
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
+import Footer from "@/components/footer";
 import Sponsors from "@/components/sponsors";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
-      <div className="mt-auto mb-auto text-white">
-        <h1 className="font-pp sm:pt-32 md:pt-64 md:text-7xl sm:text-6xl text-4xl">
-          Prismatic Motorsports
-        </h1>
-        <h2 className="md:text-3xl sm:text-xl">
-          Coming Soon
-        </h2>
+    <div className="bg-peach text-white min-h-screen flex flex-col ">
+      <div className="flex flex-col items-center text-center sm:space-y-24 mb-8">
+        <div className="text-white p-24">
+          <h1 className="font-pp sm:pt-32 md:pt-64 md:text-7xl sm:text-6xl text-4xl">
+            Prismatic Motorsports
+          </h1>
+          <h2 className="md:text-3xl sm:text-xl">
+            Coming Soon
+          </h2>
+        </div>
+        <div className="">
+          <Sponsors/>
+        </div>
       </div>
-      <div className="mt-auto pt-32">
-        <Sponsors/>
-      </div>
-    </main>
+      <Footer/>
+    </div>
   )
 }
