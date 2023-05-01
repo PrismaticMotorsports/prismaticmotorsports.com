14f32a8bba44675ab1cadef6d866391cc2d50d6b
Roxie Gibson <me@roxxers.xyz>
2023-05-01 22:22:03 +0100

    fix: good compromise for sponsor layout

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -6,15 +6,15 @@ const Sponsors = () => {
     <div className="flex flex-col items-center font-pp">
       <h1 className='text-4xl'>Our Partners</h1>
     </div>
-    <div className="flex flex-wrap flex-row mt-6 justify-center content-center gap-2 w-full sm:px-0 px-2">
+    <div className="flex flex-wrap flex-row mt-6 justify-center content-center gap-2 w-full sm:px-0">
       {sponsors.map((sponsor, i) => {
-        return <div key={i} className="flex items-center py-3 bg-white xl:w-1/5 lg:w-1/4 sm:w-1/3 h-40 p-5 mt-auto">
+        return <div key={i} className="flex items-center py-3 bg-white xl:w-1/5 lg:w-1/4 sm:w-1/3 w-3/5 h-40 p-5 mt-auto">
           <a href={sponsor.link}>
               <Image
                 style={{objectFit: 'contain'}}
-                className='w-full h-full'
-                width={1000}
-                height={500}
+                className='h-auto'
+                width={500}
+                height={250}
                 src={`/img/sponsors/${sponsor.short_name}.png`}
                 alt={`${sponsor.name} Logo`}
               />
