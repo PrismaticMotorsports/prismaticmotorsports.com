ea3ce9cac729c690b34078b367e8834ea07de18a
Roxie Gibson <me@roxxers.xyz>
2023-05-01 01:32:39 +0100

    feat: mobile layout doesnt touch edges of screen

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -6,7 +6,7 @@ const Sponsors = () => {
     <div className="flex flex-col items-center font-pp">
       <h1 className='text-4xl'>Our Partners</h1>
     </div>
-    <div className="flex flex-wrap flex-row mt-6 justify-center content-center gap-2 w-full">
+    <div className="flex flex-wrap flex-row mt-6 justify-center content-center gap-2 w-full sm:px-0 px-2">
       {sponsors.map((sponsor, i) => {
         return <div key={i} className="flex items-center py-3 bg-white xl:w-1/5 lg:w-1/4 sm:w-1/3 h-40 p-5 mt-auto">
           <a href={sponsor.link}>
